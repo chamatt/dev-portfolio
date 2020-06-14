1fc1b7e0fe2c7306249b7eedce6989e7942b9a88
ChaMatt <matt.vicent@gmail.com>
2020-06-13 23:50:36 -0300

    some fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,13 @@ import ProjectsSection from "../components/ProjectsSection";
 import BlogSection from "../components/BlogSection";
 import FooterCredits from "../components/FooterCredits";
 import AboutMe from "../components/AboutMe";
-import { attributes, react as HomeContent } from "../content/home.md";
+import * as HomeMD from "../content/home.md";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { useEffect } from "react";
 
+const { attributes, react: HomeComponent } = HomeMD;
+
 export default function Home() {
-  useEffect(() => {
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", (user) => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-        }
-      });
-    }
-  }, []);
   return (
     <div>
       <HeroSection />
@@ -29,6 +20,7 @@ export default function Home() {
       <ProjectsSection title="personal" subtitle="projects" />
       <BlogSection title="blog" subtitle="posts" />
       <FooterCredits />
+      <HomeComponent />
     </div>
   );
 }
