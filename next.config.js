90c3bd29f4d495f9a9506b03f4285adfa1f1f21d
chamatt <matt.vicent@gmail.com>
2020-06-17 02:59:06 -0300

    feat: add compression plugins

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
+const CompressionPlugin = require("compression-webpack-plugin");
+const BrotliPlugin = require("brotli-webpack-plugin"); //brotli
+
 function webpackconfig(config, options) {
   config.module.rules.push(
     {
@@ -24,6 +27,21 @@ const nextConfig = {
 module.exports = withPlugins(
   [
     [optimizedImages, {}],
+    new CompressionPlugin({
+      //gzip plugin
+      filename: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 8192,
+      minRatio: 0.8,
+    }),
+    new BrotliPlugin({
+      //brotli plugin
+      asset: "[path].br[query]",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
     // your other plugins here
   ],
   nextConfig
