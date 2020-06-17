481ce50ccf47b5f1ea34adf2fd2ae239442a9d85
chamatt <matt.vicent@gmail.com>
2020-06-17 01:18:01 -0300

    fix: remove unnamed function

--- next.config.js
+++ next.config.js
@@ -1,22 +1,24 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
+const webpackconfig = (config, options) => {
+  config.module.rules.push(
+    {
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+      options: { mode: ["react-component"] },
+    },
+    {
+      test: /\.ya?ml$/,
+      type: "json", // Required by Webpack v4
+      use: "yaml-loader",
+    }
+  );
+  return config;
+},
+
 const nextConfig = {
-  webpack: (config, options) => {
-    config.module.rules.push(
-      {
-        test: /\.md$/,
-        loader: "frontmatter-markdown-loader",
-        options: { mode: ["react-component"] },
-      },
-      {
-        test: /\.ya?ml$/,
-        type: "json", // Required by Webpack v4
-        use: "yaml-loader",
-      }
-    );
-    return config;
-  },
+  webpack: webpackconfig
 };
 
 module.exports = withPlugins(
